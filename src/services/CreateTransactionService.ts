d6068df5a69e47194efb22d36744399d53086679
jemsantos <eustaquio.cadastros@gmail.com>
2020-11-06 23:20:13 -0300

    :s parkles: Desafio 05 resolvido e entregue

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,17 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+/* {
+  "id": "uuid",
+  "title": "Salário",
+  "value": 3000,
+  "type": "income"
+} */
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +19,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type is invalid');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw Error('You do not have enough balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
